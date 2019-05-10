66722c14858f76afcb60e4956910a6ff26962595
Gazuka <jerome.carion@gmail.com>
2019-05-10 17:01:28 +0200

    Ck finder

--- src/Controller/AdminPersonnelController.php
+++ src/Controller/AdminPersonnelController.php
@@ -8,9 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminPersonnelController extends AbstractController
 {
     /**
-     * @Route("/admin/personnel", name="admin_personnel")
+     * @Route("/admin/personnel2", name="admin_personnel")
      */
     public function index()
+    {
+        return $this->render('admin/admin_personnel/index.html.twig', [
+            'controller_name' => 'AdminPersonnelController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/test", name="persotest")
+     */
+    public function index2()
     {
         return $this->render('admin_personnel/index.html.twig', [
             'controller_name' => 'AdminPersonnelController',
